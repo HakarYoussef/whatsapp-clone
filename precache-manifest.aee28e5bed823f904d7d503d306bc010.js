226d388450564ac8268c947e121596a56dfe1e79
HakarYoussef <hakar.yusuf@gmail.com>
2020-09-06 01:40:18 +0300

    Updates

---
+++ precache-manifest.aee28e5bed823f904d7d503d306bc010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afcca690d2bcacd6a06657b4e0162eef",
+    "url": "/HakarYoussef/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "8a4a1ad34722d56e6d78",
+    "url": "/HakarYoussef/whatsapp-clone/static/css/main.1479aada.chunk.css"
+  },
+  {
+    "revision": "8efa018d032c65cfd734",
+    "url": "/HakarYoussef/whatsapp-clone/static/js/2.f71ad3da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HakarYoussef/whatsapp-clone/static/js/2.f71ad3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4a1ad34722d56e6d78",
+    "url": "/HakarYoussef/whatsapp-clone/static/js/main.10655a09.chunk.js"
+  },
+  {
+    "revision": "9c2c71bcc4edcb3acc95",
+    "url": "/HakarYoussef/whatsapp-clone/static/js/runtime-main.f353fd6c.js"
+  }
+]);
